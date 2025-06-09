43899506f2092f1f292bbde5e66016a5ba52b9d2
Su Yue <77612121+lsuyueees@users.noreply.github.com>
2025-06-09 23:39:57 +0800

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-export const Auth0Config = {
-  domain: 'YOUR_AUTH0_DOMAIN', // e.g., 'your-domain.auth0.com'
-  clientId: 'YOUR_CLIENT_ID', // from Auth0 dashboard
-  redirectUri: 'yourredirecturi', // from Auth0 dashboard
-  audience: 'https://your-auth0-domain/api/v2/', // audience for API calls
-  scope: 'openid profile email', // adjust scopes as needed
-};
